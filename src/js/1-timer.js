411fc2f770d7fc8140c65fcacad3d6b1e39102ad
Anna Balesko <babasheldona@icloud.com>
2025-10-10 01:48:25 -1100

    add timer styles

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,69 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/izitoast.min.css';
 
+const inputTime = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+
+const daysEl = document.querySelector('span[data-days]');
+const hoursEl = document.querySelector('span[data-hours]');
+const minsEl = document.querySelector('span[data-minutes]');
+const secsEl = document.querySelector('span[data-seconds]');
+
+let userSelectedDate = null;
+let timerId = null;
+
+startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedTimes = selectedDates[0];
+      if (selectedTimes > new Date()) {
+         startBtn.disabled = false;
+          userSelectedDate += selectedTimes;
+      } else {
+          startBtn.disabled = true;
+          userSelectedDate = null;
+           iziToast.error({
+              title: 'Invalid date',
+              message: 'Please choose a date in the future',
+              position: 'topRight',
+          });
+      }
+  },
+};
+
+flatpickr(inputTime, options);
+
+startBtn.addEventListener('click', start);
+start();
+function start() {
+  if (userSelectedDate) {
+      startBtn.disabled = false;
+  }
+}
+
+
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
